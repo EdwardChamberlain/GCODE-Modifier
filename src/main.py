910ccfb719f2102031f7be70b2933eb6bf163f6d
Edward Chamberlain <edward.chamberlain@me.com>
2022-08-01 15:26:55 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import json
+
+with open(r'src/config.json', 'r') as f:
+    config = json.loads(f.read())
+
+with open(r'examples/PA-on-and-off-trial.gcode', 'r') as f:
+    file = f.readlines()
+
+find_replace = config['find-append']
+for k, v in find_replace.items():
+    print(f"{k} -> {v}")
+    file = [
+        i if i.strip() != k else i + v + " ;ADDED BY GCODE EDITOR" + "\n"
+        for i in file
+    ]
+
+with open(r'examples/PA-on-and-off-trial_output.gcode', 'w') as f:
+    f.writelines(file)
