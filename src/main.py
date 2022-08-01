808180c012aba4940d3321510a76bc117c7e07e9
Edward Chamberlain <edward.chamberlain@me.com>
2022-08-01 16:37:04 +0100

    Converted to functions

--- src/main.py
+++ src/main.py
@@ -1,18 +1,33 @@
 import json
 
 
-with open(r'src/config.json', 'r') as f:
-    config = json.loads(f.read())
+def read_config(path=r'src/config.json'):
+    with open(path, 'r') as f:
+        config = json.loads(f.read())
+    return config
 
-with open(r'examples/PA-on-and-off-trial.gcode', 'r') as f:
-    file = f.readlines()
 
-find_replace = config['find-append']
-for k, v in find_replace.items():
-    file = [
-        i if i.strip() != k else i + v + " ;ADDED BY GCODE EDITOR" + "\n"
-        for i in file
-    ]
+def find_replace(file, replace_terms):
+    for k, v in replace_terms.items():
+        file = [
+            i if i.strip() != k else i + v + " ;ADDED BY GCODE EDITOR" + "\n"
+            for i in file
+        ]
+    return file
 
-with open(r'examples/PA-on-and-off-trial_output.gcode', 'w') as f:
-    f.writelines(file)
+
+def read_gcode(filepath):
+    with open(filepath, 'r') as f:
+        file = f.readlines()
+    return file
+
+
+if __name__ == '__main__':
+    config = read_config()
+    file = read_gcode(r'examples/PA-on-and-off-trial.gcode')
+
+    replace_terms = config['find-append']
+    file = find_replace(file, replace_terms)
+
+    with open(r'examples/PA-on-and-off-trial_output.gcode', 'w') as f:
+        f.writelines(file)
